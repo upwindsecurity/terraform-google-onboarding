f60be49ef4f6213af50e30675f1e67673e831205
Adam Humphreys <ahumph@users.noreply.github.com>
2025-06-26 13:15:26 +0100

    feat(AG-2991): add storage bucket read permissions to management account (#19)

--- modules/organization/iam-roles.tf
+++ modules/organization/iam-roles.tf
@@ -10,3 +10,44 @@ resource "google_organization_iam_member" "upwind_management_sa_org_viewer_role_
   role   = "roles/viewer"
   member = "serviceAccount:${google_service_account.upwind_management_sa.email}"
 }
+
+# Specifically grant permissions to read storage objects
+# This is a reduction from the storageAdmin role to grant read only access
+resource "google_organization_iam_custom_role" "storage_reader_role" {
+  org_id      = data.google_organization.org.org_id
+  role_id     = "UpwindStorageReader_${local.resource_suffix_underscore}"
+  title       = "Upwind Management SA Storage Reader"
+  description = "Custom role for Upwind Management Service Account to read storage buckets."
+
+  permissions = [
+    "storage.buckets.get",
+    "storage.buckets.getIamPolicy",
+    "storage.buckets.getIpFilter",
+    "storage.buckets.list",
+    "storage.buckets.listEffectiveTags",
+    "storage.buckets.listTagBindings",
+    "storage.bucketOperations.get",
+    "storage.bucketOperations.list",
+    "storage.folders.get",
+    "storage.folders.list",
+    "storage.managedFolders.get",
+    "storage.managedFolders.getIamPolicy",
+    "storage.managedFolders.list",
+    "storage.objects.getIamPolicy",
+    "storage.objects.list",
+  ]
+}
+
+resource "google_organization_iam_member" "upwind_management_sa_storage_reader_role_member" {
+  org_id = data.google_organization.org.org_id
+  role   = google_organization_iam_custom_role.storage_reader_role.id
+  member = "serviceAccount:${google_service_account.upwind_management_sa.email}"
+}
+
+# Required for DSPM functionality when cloudscanners are enabled
+resource "google_organization_iam_member" "cloudscanner_sa_storage_reader_role_member" {
+  count  = var.enable_cloudscanners ? 1 : 0
+  org_id = data.google_organization.org.org_id
+  role   = google_organization_iam_custom_role.storage_reader_role.id
+  member = "serviceAccount:${google_service_account.cloudscanner_sa[0].email}"
+}
