379f2baf7bbf3c4184bf27cb48beb7aa4ce5adbf
ahumph <adam.humphreys@upwind.io>
2025-06-27 12:18:28 +0100

    chore(AG-3006): create backport with terraform version 1.9.5

--- modules/organization/secrets.tf
+++ modules/organization/secrets.tf
@@ -34,23 +34,23 @@ resource "google_secret_manager_secret" "scanner_client_secret" {
   }
 }
 resource "google_secret_manager_secret_version" "upwind_client_id_v1" {
-  secret         = google_secret_manager_secret.upwind_client_id.id
-  secret_data_wo = var.upwind_client_id
+  secret      = google_secret_manager_secret.upwind_client_id.id
+  secret_data = var.upwind_client_id
 }
 
 resource "google_secret_manager_secret_version" "upwind_client_secret_v1" {
-  secret         = google_secret_manager_secret.upwind_client_secret.id
-  secret_data_wo = var.upwind_client_secret
+  secret      = google_secret_manager_secret.upwind_client_secret.id
+  secret_data = var.upwind_client_secret
 }
 
 resource "google_secret_manager_secret_version" "scanner_client_id_v1" {
-  count          = var.enable_cloudscanners ? 1 : 0
-  secret         = google_secret_manager_secret.scanner_client_id[0].id
-  secret_data_wo = var.scanner_client_id
+  count       = var.enable_cloudscanners ? 1 : 0
+  secret      = google_secret_manager_secret.scanner_client_id[0].id
+  secret_data = var.scanner_client_id
 }
 
 resource "google_secret_manager_secret_version" "scanner_client_secret_v1" {
-  count          = var.enable_cloudscanners ? 1 : 0
-  secret         = google_secret_manager_secret.scanner_client_secret[0].id
-  secret_data_wo = var.scanner_client_secret
+  count       = var.enable_cloudscanners ? 1 : 0
+  secret      = google_secret_manager_secret.scanner_client_secret[0].id
+  secret_data = var.scanner_client_secret
 }
