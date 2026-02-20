d4497ffd004a7bc66ab80c34b1ae309c434481e4
Michael Eller <michael.eller@upwind.io>
2026-02-20 09:21:48 +0000

    chore(AG-3652): Add label to service account which indicates if dspm enabled

--- modules/organization/shared.tf
+++ modules/organization/shared.tf
@@ -9,6 +9,7 @@ module "iam" {
   scanner_client_secret       = var.scanner_client_secret
   resource_suffix             = var.resource_suffix
   enable_cloudscanners        = var.enable_cloudscanners
+  enable_dspm_scanning        = var.enable_dspm_scanning
   labels                      = var.labels
 
   google_service_account_display_name = var.google_service_account_display_name
